6f41c363bbc3efe788106948ce86b51d0475496b
Tim Krebs <tim.krebs@hashicorp.com>
2025-08-15 10:47:23 +0200

    Add EKS module with Terraform configuration for AWS EKS cluster setup
    - Introduced `main.tf` to define the EKS cluster, IAM roles, and node groups.
    - Added `outputs.tf` to expose key attributes of the EKS cluster and node groups.
    - Created `user_data.sh` for initializing nodes with monitoring tools and cost optimization scripts.
    - Defined variables in `variables.tf` for customizable EKS configurations, including node groups and security settings.
    - Implemented IAM roles and policies for the AWS Load Balancer Controller and Cluster Autoscaler.

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,79 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "control_plane_subnet_ids" {
+  description = "List of subnet IDs for the EKS control plane"
+  type        = list(string)
+}
+
+variable "cluster_security_group_id" {
+  description = "Security group ID for the EKS cluster"
+  type        = string
+}
+
+variable "node_security_group_id" {
+  description = "Security group ID for the EKS worker nodes"
+  type        = string
+}
+
+variable "kms_key_id" {
+  description = "KMS key ID for EKS encryption"
+  type        = string
+}
+
+variable "node_groups" {
+  description = "Map of EKS node group configurations"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    min_size      = number
+    max_size      = number
+    desired_size  = number
+    disk_size     = number
+    labels        = map(string)
+    taints        = list(object({
+      key    = string
+      value  = string
+      effect = string
+    }))
+  }))
+  default = {}
+}
+
+variable "cluster_addons" {
+  description = "Map of EKS cluster add-ons"
+  type = map(object({
+    version = string
+  }))
+  default = {}
+}
+
+variable "enable_spot_instances" {
+  description = "Enable spot instances for cost optimization"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
